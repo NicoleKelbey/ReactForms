319523244ce5e5929fbaaa7541a10a4b51502c96
NicoleKelbey <nikki.markert@yahoo.com>
2024-11-04 10:02:46 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
+import SignUpForm from "./components/SignUpForm";
+import Authenticate from "./components/Authenticate";
+import { useState } from "react";
 
-import './App.css'
+const App = () => {
+  const [token, setToken] = useState('');
 
-function App() {
- 
+  console.log("Current token:", token);
 
   return (
     <>
-      
+      <SignUpForm setToken={setToken} />
+      {token ? (
+        <>
+          <p>Token: {token}</p> {/* Temporary display of token for debugging */}
+          <Authenticate token={token} />
+        </>
+      ) : (
+        <p style={{ color: 'red' }}>Please sign up to obtain a token.</p>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
